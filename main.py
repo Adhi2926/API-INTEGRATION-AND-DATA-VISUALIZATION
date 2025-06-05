bb2461c6e37fc31fde1cf4010e864f724f479007
Adhi2926 <gbalaswamiadhigoud@gmail.com>
2025-06-05 15:57:10 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-from flask import Flask, request, jsonify 
+import matplotlib.pyplot as plt
 
-app = Flask(__name__)
+data={
+    'sem 1': 9.2,
+    'sem 2': 8.9,
+    'sem 3': 9.7,
+    'sem 4': 8.1,
+    'sem 5': 6.6,
+    'sem 6': 9.4
+}
 
-@app.route("/")
-def home():
-    return "Home"
+SEM=list(data.keys());
+SPGA=list(data.values());
 
-
-if __name__== "__main__":
-    app.run(debug=True)
+fig=plt.figure(figsize=(10, 5))
+plt.bar(SEM,SPGA, color="blue", width=0.3)
+plt.xlabel('sem')
+plt.ylabel('sgpa')
+plt.title("sem and sgpa")
+plt.show()
